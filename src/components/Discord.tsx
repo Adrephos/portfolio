e807ef2563cc2eeb89de202266fdb8408604fbd3
Adrephos <adrephos@gmail.com>
2023-01-13 22:51:37 -0500

    feat: add Discord and Spotify components

--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -10,60 +10,27 @@ export const Discord = ({
     (activity) => activity.type === 0
   );
 
-  const [statusColor, statusSubColor] =
+  const statusColor =
     lanyard && lanyard.discord_status === "online"
-      ? ["#43B581", "#2E8D62"]
+      ? "#31A254"
       : lanyard && lanyard.discord_status === "idle"
-      ? ["#FAA61A", "#B57B1B"]
+      ? "#FAA61A"
       : lanyard && lanyard.discord_status === "dnd"
-      ? ["#F04747", "#A32323"]
-      : ["#565656", "#3F3F3F"];
+      ? "#CA383B"
+      : "#2C2F33"
 
   const [style, setStyle] = useState({});
 
   useEffect(() => {
     setStyle({
       backgroundColor: statusColor,
-      boxShadow: `0rem 0.3rem ${statusSubColor}`,
+	  'border-color': statusColor,
+	  'border-width': '0.2rem',
     });
   }, [statusColor]);
 
   return (
     <div className="flex flex-col items-center">
-      <a
-        href="https://discord.com/users/432958582536011777"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div
-          className="max-w-max rounded-lg p-2 px-5 font-bold text-secondary transition-all hover:-translate-y-1 active:translate-y-1"
-          style={style}
-          onMouseEnter={() =>
-            setStyle({
-              backgroundColor: statusColor,
-              boxShadow: `0rem 0.5rem ${statusSubColor}`,
-            })
-          }
-          onMouseLeave={() =>
-            setStyle({
-              backgroundColor: statusColor,
-              boxShadow: `0rem 0.3rem ${statusSubColor}`,
-            })
-          }
-          onPointerDown={() =>
-            setStyle({
-              backgroundColor: statusColor,
-              boxShadow: `0rem 0rem ${statusSubColor}`,
-            })
-          }
-        >
-          {lanyard
-            ? lanyard.discord_status.charAt(0).toUpperCase() +
-              lanyard.discord_status.slice(1)
-            : "Offline"}{" "}
-          on Discord
-        </div>
-      </a>
       <div className="my-5 flex h-[9.37rem] min-w-[18.75rem] px-5 items-center justify-center rounded-lg bg-[#1F1F1F]">
         {mainActivity ? (
           <div className="flex flex-row items-center">
@@ -99,6 +66,43 @@ export const Discord = ({
           </p>
         )}
       </div>
+	  <a
+        href="https://discord.com/users/432958582536011777"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div
+			className="max-w-max rounded-lg p-2 px-5 font-bold text-secondary transition-all hover:-translate-y-1 active:translate-y-1"
+          style={style}
+          onMouseEnter={() =>
+            setStyle({
+			  'border-width': '0.2rem',
+			  'border-color': statusColor,
+			  color: statusColor,
+            })
+          }
+          onMouseLeave={() =>
+            setStyle({
+			  'border-width': '0.2rem',
+			  'border-color': statusColor,
+              backgroundColor: statusColor,
+            })
+          }
+          onPointerDown={() =>
+            setStyle({
+			  'border-width': '0.2rem',
+			  'border-color': statusColor,
+			  color: statusColor,
+            })
+          }
+        >
+          {lanyard
+            ? lanyard.discord_status.charAt(0).toUpperCase() +
+              lanyard.discord_status.slice(1)
+            : "Offline"}{" "}
+          on Discord
+        </div>
+      </a>
     </div>
   );
 };
--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -6,12 +6,17 @@ export const Spotify = ({
   spotify: lanyardSpotify | undefined | null;
 }) => {
   return spotify ? (
-    <div className="flex h-[8rem] max-w-[25rem] items-center justify-center rounded-lg bg-[#1F1F1F] p-5">
-      <img
-        src={spotify.album_art_url ? spotify.album_art_url : ''}
-        alt="activity"
-        className="mr-3 h-[5.5rem] w-[5.5rem] rounded-lg transition duration-500 hover:skew-y-6"
-      />
+    <div className="flex h-[8rem] max-w-[25rem] items-center justify-center rounded-lg">
+	  <a
+		  href={`https://open.spotify.com/track/${spotify.track_id}`}
+		  rel="noopener noreferrer"
+		  target="_blank">
+		  <img
+			src={spotify.album_art_url ? spotify.album_art_url : ''}
+			alt="activity"
+			className="mr-3 h-[5.5rem] w-[5.5rem] rounded-lg transition duration-500 hover:skew-y-6"
+		  />
+	  </a>
       <div className="flex flex-col truncate text-secondary">
         <p className="text-[0.9rem] font-bold text-[#1ed760]">
           Listening to spotify...
