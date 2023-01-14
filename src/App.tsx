ec73a9f21a1c215057dcdc3af819999e7a37486a
Adrephos <adrephos@gmail.com>
2023-01-13 22:56:06 -0500

    feat: put everything together

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import './App.css';
 import { useLanyardWS } from "use-lanyard";
 import { Discord } from "./components/Discord"
 import { Footer } from "./components/Footer"
-import './App.css';
+import { Projects } from "./components/Projects"
+import { Playlists } from "./components/Playlists"
+import { About } from "./components/About";
+import { TopBar } from "./components/TopBar";
 
 const DISCORD_ID = "432958582536011777";
 
@@ -9,9 +13,19 @@ export const App = () => {
   const data = useLanyardWS(DISCORD_ID);
 
   return (
-	  <div>
-		<Discord lanyard={data} />
-		<Footer spotify={data?.spotify} />
+	  <div className="flex flex-col bg-gray h-auto w-auto gap-5 md:gap-0 md:h-screen">
+		  <TopBar discordUser={data?.discord_user}/>
+		  <div className="lg:flex flex-row-reverse h-full justify-around">
+			<div className="flex flex-wrap p-5 md:border-l-2 border-green justify-center h-full md:p-5 md:pl-10 md:flex-col">
+			  <About/>
+			  <Discord lanyard={data} />
+			</div>
+			  <div className="flex flex-wrap gap-5 py-10 px-5 items-center justify-around h-full md:gap-0 md:p-0 md:flex-col">
+			  <Projects/>
+			  <Playlists/>
+			  <Footer spotify={data?.spotify} />
+			</div>
+		</div>
 	  </div>
 	);
 }
