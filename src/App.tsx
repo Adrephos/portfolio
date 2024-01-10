aff1018cf2f91e20f03e816194669fc2424df24f
Adrephos <adrephos@gmail.com>
2024-01-10 10:46:24 -0500

    fix: resolve display issue on specific screens

--- src/App.tsx
+++ src/App.tsx
@@ -36,11 +36,11 @@ export const App = () => {
 				<TopBar/>
 			</div>
 			<div className="flex flex-wrap flex-row-reverse h-full justify-around">
-				<div className="flex flex-col p-5 lg:w-2/5 lg:border-l-2 items-center justify-center h-full lg:p-0" style={style}>
+				<div className="flex flex-col p-5 lg:w-2/5 lg:border-l-2 items-center justify-center h-auto" style={style}>
 					<About statusColor={statusColor} />
 					<Discord lanyard={data} statusColor={statusColor} />
 				</div>
-				<div className="flex flex-col lg:w-3/5 py-0 px-5 items-center justify-around h-full lg:p-0">
+				<div className="flex flex-col lg:w-3/5 py-0 px-5 items-center justify-around h-auto">
 					<Projects statusColor={statusColor} />
 					<Playlists statusColor={statusColor} />
 					<Footer lanyard={data} />
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,57 +1,57 @@
 import {
-  DiscordIcon,
-  GitHubIcon,
-  SteamIcon,
-  LinkedInIcon,
+	DiscordIcon,
+	GitHubIcon,
+	SteamIcon,
+	LinkedInIcon,
 } from "./Icons";
 
 export const TopBar = () => {
-  return (
-	  <div className="flex flex-wrap items-center justify-center py-3 px-5 sm:justify-between md:px-10">
-	  <div className="flex justify-center items-center gap-5">
-		<img
-		  src="https://github.com/adrephos.png"
-		  alt="avatar"
-		  className="h-[3.5rem] w-[3.5rem] rounded-lg"
-		/>
-		<a href="/" className="text-secondary hover:text-primary">
-          <p className="text-[2rem] font-bold">Blog</p>
-        </a>
-      </div>
-      <div className="flex w-[12.5rem] justify-around">
-        <a
-          href="https://discord.com/users/432958582536011777"
-          target="_blank"
-          rel="noopener noreferrer"
-		  className="hover:-translate-y-1 active:translate-y-1"
-        >
-          <DiscordIcon />
-        </a>
-        <a
-          href="https://github.com/Adrephos"
-          target="_blank"
-          rel="noopener noreferrer"
-		  className="hover:-translate-y-1 active:translate-y-1"
-        >
-          <GitHubIcon />
-        </a>
-        <a
-          href="https://steamcommunity.com/id/adrephos"
-          target="_blank"
-          rel="noopener noreferrer"
-		  className="hover:-translate-y-1 active:translate-y-1"
-        >
-          <SteamIcon />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/adrephos/"
-          target="_blank"
-          rel="noopener noreferrer"
-		  className="hover:-translate-y-1 active:translate-y-1"
-        >
-          <LinkedInIcon />
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-wrap items-center justify-center py-3 px-5 sm:justify-between md:px-10">
+			<div className="flex justify-center items-center gap-5 p-2 sm:p-0">
+				<img
+					src="https://github.com/adrephos.png"
+					alt="avatar"
+					className="h-[3.5rem] w-[3.5rem] rounded-lg"
+				/>
+				<a href="/" className="text-secondary hover:text-primary">
+					<p className="text-[2rem] font-bold">Blog</p>
+				</a>
+			</div>
+			<div className="flex w-[12.5rem] justify-around p-2 sm:p-0">
+				<a
+					href="https://discord.com/users/432958582536011777"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="hover:-translate-y-1 active:translate-y-1"
+				>
+					<DiscordIcon />
+				</a>
+				<a
+					href="https://github.com/Adrephos"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="hover:-translate-y-1 active:translate-y-1"
+				>
+					<GitHubIcon />
+				</a>
+				<a
+					href="https://steamcommunity.com/id/adrephos"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="hover:-translate-y-1 active:translate-y-1"
+				>
+					<SteamIcon />
+				</a>
+				<a
+					href="https://www.linkedin.com/in/adrephos/"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="hover:-translate-y-1 active:translate-y-1"
+				>
+					<LinkedInIcon />
+				</a>
+			</div>
+		</div>
+	);
 };
