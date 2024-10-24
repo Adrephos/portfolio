89683ec38cfbaa21651e56e9b94f2b3bd62ed0bb
Adrephos <adrephos@gmail.com>
2024-10-24 12:33:16 -0500

    feat: make age auto-update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,27 @@
 import { TechStack } from "./TechStack";
 
-export const About = ({statusColor} : { statusColor: string | undefined }) => {
+export const About = ({ statusColor }: { statusColor: string | undefined }) => {
+  const today = new Date();
+  const ageMillis = today.getTime() - new Date('2003-12-19').getTime();
+  const age = Math.floor(ageMillis / 31556952000);
+
+
   return (
     <div className="text-secondary items-center">
       <p>
-        J.Esteban, 19 years old. &ensp;&ensp;&ensp;
+        J.Esteban, {age} years old. &ensp;&ensp;&ensp;
         <span className="text-[0.75rem] font-bold">Español & English</span>
         <br />
         Computer Science student and back-end developer
         <br />
         <br />
-		  Experience using Linux and High performance Computing
-		  <br/> I enjoy videogames and music as well as coding and learning
+        Experience using Linux and High performance Computing
+        <br /> I enjoy videogames and music as well as coding and learning
       </p>
       <br />
       <p>Technologies I commonly use:</p>
       <br />
-      <TechStack statusColor={statusColor}/>
+      <TechStack statusColor={statusColor} />
       <br />
     </div>
   );
